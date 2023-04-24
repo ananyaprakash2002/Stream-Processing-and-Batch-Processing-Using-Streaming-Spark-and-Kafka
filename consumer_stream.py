0220cc570a377f79d853190b91bf0e9f449b387f
Ananya <79403627+ananyaprakash2002@users.noreply.github.com>
2023-04-24 18:17:57 +0530

    Update files

--- consumer_stream.py
+++ consumer_stream.py
@@ -1,9 +1,14 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import split, window
+from pyspark.sql.functions import window
 import pyspark.sql.functions as func
+from pyspark.sql.functions import split, current_timestamp
 
-spark = SparkSession.builder.appName("test").getOrCreate()
 
+spark = SparkSession.builder \
+    .appName("test1") \
+    .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.4.0") \
+    .getOrCreate()
+    
 # Read data from the Kafka topic
 df = spark \
     .readStream \
@@ -14,23 +19,27 @@ df = spark \
     .option("includeHeaders", "true") \
     .load()
 
+print(" Consumer Stream Processing has Started")
+
 # Extract the tweet and hashtags from the Kafka message value
-df1 = df.selectExpr("CAST(value AS STRING)", "topic") \
-        .withColumn('tweet', split(df['value'], '\s+', limit=3).getItem(1)) \
-        .withColumn('hashtags', split(df['value'], '\s+', limit=3).getItem(2))
+df1 = df.selectExpr("CAST(value AS STRING)", "topic")
+df2 = df1.withColumn('tweet', split(df1['value'], '\s+', limit=3).getItem(1)) \
+        .withColumn('hashtags', split(df1['value'], '\s+', limit=3).getItem(2)) \
+        .withColumn("timestamp", current_timestamp()) 
 
 # Create a temporary view of the dataframe
-df1.createOrReplaceTempView("tweets")
+df2.createOrReplaceTempView("tweets")
 
-# Filter the dataframe to include only tweets with a particular hashtag and positive topic
+print("Temporary view has been created!")
+# Filtering dataframe to include only tweets with hashtag and having topic as positive
 filtered_df = spark.sql("SELECT * FROM tweets WHERE hashtags != '' AND topic = 'positive'")
 
-# Group the tweets by a 15-minute window and count the number of tweets in each window
+# Grouping tweets by a 30-minute window and counting the number of tweets in each window
 count_df = filtered_df \
-            .groupBy(window("timestamp", "15 minutes")) \
+            .groupBy(window("timestamp","30 minutes")) \
             .agg(func.count("hashtags").alias("count"))
 
-# Start the streaming query to write the results to the console
+# Starting the streaming query to write the results to the console
 query = count_df \
     .writeStream \
     .outputMode("complete") \
